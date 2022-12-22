bbe79b6154a78fceadc326fbe439ea89594128e3
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-22 15:07:14 -0400

    Completed RegisterActivity layout

--- app/src/main/java/com/example/speedtypinggame/SplashScreen.java
+++ app/src/main/java/com/example/speedtypinggame/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.speedtypinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this, RegisterActivity.class));
+            finish();
+        }, 1000);
     }
 }
